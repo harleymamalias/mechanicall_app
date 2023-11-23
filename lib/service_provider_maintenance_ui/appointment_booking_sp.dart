006a5c28213a263d49674df39679a1e638d4e7ce
Harley Mamalias <harleymamalias@gmail.com>
2023-11-23 21:11:17 +0800

    added initial ui for service provider appointment booking UI

---
+++ lib/service_provider_maintenance_ui/appointment_booking_sp.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BookingAppointmentUIServiceProvider extends StatelessWidget {
+  const BookingAppointmentUIServiceProvider({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
