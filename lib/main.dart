70834b87728a889347790889d7f7547950211faa
Harley Mamalias <harleymamalias@gmail.com>
2023-11-24 01:46:06 +0800

    remove unused import

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'login_and_registration/launch_screen.dart';
-import 'payments/arrival_of_mechanic_page.dart';
-import 'service_provider_maintenance_ui/service_provider_homepage.dart';
 
 void main() {
   runApp(const MyApp());
